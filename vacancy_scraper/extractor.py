8b7ada9cdf3e9af2316a23a30b0ff3ca3ba49a78
semmber <sillabika@gmail.com>
2025-08-10 17:59:34 +0300

    изменила извлечение зарплаты, проф роли, формата работы, часов работы. добавила извлечение требуемого опыта

--- vacancy_scraper/extractor.py
+++ vacancy_scraper/extractor.py
@@ -6,11 +6,14 @@ def data_extractor(vacancies:dict, headers:dict):
     for vac in vacancies["items"]:
         print(vac["name"])
         id_vac = vac["id"]
-        skills = []
         full_vac = requests.get(f"https://api.hh.ru/vacancies/{id_vac}", headers=headers).json()
+        print(json.dumps(full_vac, indent=4, ensure_ascii=False))
         print(extract_key_skills(full_vac))
         print(extract_salary(full_vac))
+        print(extract_professional_role(full_vac))
+        print(extractor_work_format(vac))
         print(extractor_work_hours(vac))
+        print(extractor_experience(vac))
         print()
         time.sleep(0.25)
     return
@@ -20,38 +23,47 @@ def extract_key_skills(vacancy:dict) -> list | None:
     if vacancy["key_skills"] is None:
         return None
     skills = [skill["name"] for skill in vacancy["key_skills"]]
-    return skills
+    return vacancy["key_skills"]
 
 
-def extract_salary(vacancy:dict) -> int | None:
+def extract_salary(vacancy:dict) -> dict | None:
     if vacancy["salary"] is None:
         return None
-    salary_from = vacancy["salary"]["from"]
-    salary_to = vacancy["salary"]["from"]
+    salary_from = int(vacancy["salary"]["from"])
+    salary_to = vacancy["salary"]["to"] if vacancy["salary"]["to"] is not None else 0
     if salary_from and salary_to:
-        return int((salary_from + salary_to) / 2)
+        salary_avg = int((salary_from + salary_to) / 2)
+        return {"salary_from":salary_from, "salary_to":salary_to, "salary_avg":salary_avg}
     elif salary_from:
-        return salary_from
+        return {"salary_from":salary_from}
     elif salary_to:
-        return salary_to
+        return {"salary_to":salary_to}
     else:
         return None
 
 
-def extract_professional_role(vacancy:dict) -> str | None:
+def extract_professional_role(vacancy:dict) -> list | None:
     if vacancy["professional_roles"] is None:
         return None
-    return vacancy["professional_roles"]["name"]
+    return [pr for pr in vacancy["professional_roles"]]
 
 
 def extractor_work_format(vacancy:dict) -> list | None:
     if vacancy["work_format"] is None:
         return None
-    formats = [wf["name"] for wf in vacancy["work_format"]]
+    formats = [{"id":wf["id"], "name":wf["name"].replace("\xa0", " ")} for wf in vacancy["work_format"]]
     return formats
 
 
-def extractor_work_hours(vacancy:dict):
+def extractor_work_hours(vacancy:dict) -> list | None:
     if vacancy["working_hours"] is None:
         return None
-    return vacancy["working_hours"]
+    list_hours = [i["id"] for i in vacancy["working_hours"]]
+    return list_hours
+
+
+def extractor_experience(vacancy:dict) -> dict | None:
+    if vacancy["experience"] is None:
+        return None
+    return vacancy["experience"]
+

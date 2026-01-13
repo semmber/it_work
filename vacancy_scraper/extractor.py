af5320ddd83232d4de4ef40022630a1084093ccf
semmber <sillabika@gmail.com>
2026-01-13 21:22:07 +0300

    просто добавила get в extractor

--- vacancy_scraper/extractor.py
+++ vacancy_scraper/extractor.py
@@ -7,7 +7,7 @@ def data_extractor(vacancies:list, headers:dict):
     for vac in vacancies:
         id_vac = int(vac["id"])
         full_vac = requests.get(f"https://api.hh.ru/vacancies/{id_vac}", headers=headers).json()
-        profession = score_profession(full_vac["name"], full_vac["description"], extract_key_skills(full_vac))
+        profession = score_profession(full_vac.get("name", ""), full_vac.get("description", ""), extract_key_skills(full_vac))
         if profession[0] != '':
             dt = []
             dt.extend([id_vac, profession[0], extract_experience(vac),
@@ -20,7 +20,7 @@ def data_extractor(vacancies:list, headers:dict):
 
 
 def extract_key_skills(vacancy:dict) -> list | None:
-    if vacancy["key_skills"] is None:
+    if vacancy.get("key_skills") is None:
         return None
     return [(v["name"],) for v in vacancy["key_skills"]]
 
@@ -43,19 +43,19 @@ def extract_salary(vacancy:dict) -> dict | None:
 
 
 def extract_work_format(vacancy:dict) -> list | None:
-    if vacancy["work_format"] is None:
+    if vacancy.get("work_format") is None:
         return None
     formats = [(wf["id"], wf["name"].replace("\xa0", " ")) for wf in vacancy["work_format"]]
     return formats
 
 
 def extract_experience(vacancy:dict) -> dict | None:
-    if vacancy["experience"] is None:
+    if vacancy.get("experience") is None:
         return None
     return vacancy["experience"]
 
 
 def extract_date(vacancy:dict) -> str | None:
-    if vacancy["published_at"] is None:
+    if vacancy.get("published_at") is None:
         return None
     return vacancy["published_at"]
